7fa4926ebc87d145259dad8a757f5ffc7aeb5192
Wiley Wu <wiley12555@gmail.com>
2018-06-13 17:41:27 -0400

    Add hortonwork Practice question

---
+++ src/flightdelay/join/practice/Task1.java
@@ -0,0 +1,55 @@
+package flightdelay.join.practice;
+
+
+/**
+
+ For example, for the date 2008-01-03, there is a delayed flight number 488 from Las Vegas (LAS) to San Francisco (SFO). The corresponding output would be:
+ 2008,1,3,1426,1605,WN,488,99,35,31,LAS,SFO,43,150,94
+ The output is sorted by date ascending, and on each day the output is sorted by ArrDelay descending (so that the longest arrival delays appear first).
+ The output is in text files in a new folder in HDFS named task1 with values separated by commas
+ The output is in two text files
+ Data:
+ -------------------------sfo_weather.csv ---------------------------------------
+ STATION_NAME,YEAR,MONTH,DAY,PRCP,TMAX,TMIN
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,01,0,122,39
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,02,0,117,39
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,03,43,150,94
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,04,533,150,100
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,05,196,122,78
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,06,15,106,50
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,07,0,111,67
+ SAN FRANCISCO INTERNATIONAL AIRPORT CA US,2008,01,08,20,128,61
+ -------------------------flight_delays1?2?3.csv----------------------------------
+ Year,Month,DayofMonth,DayOfWeek,DepTime,CRSDepTime,ArrTime,CRSArrTime,UniqueCarrier,FlightNum,TailNum,
+ ActualElapsedTime,CRSElapsedTime,AirTime,ArrDelay,DepDelay,Origin,Dest,Distance,TaxiIn,TaxiOut,Cancelled,
+ CancellationCode,Diverted,CarrierDelay,WeatherDelay,NASDelay,SecurityDelay,LateAircraftDelay
+ 2008,1,3,4,2003,1955,2211,2225,WN,335,N712SW,128,150,116,-14,8,IAD,TPA,810,4,8,0,,0,NA,NA,NA,NA,NA
+ 2008,1,3,4,754,735,1002,1000,WN,3231,N772SW,128,145,113,2,19,IAD,TPA,810,5,10,0,,0,NA,NA,NA,NA,NA
+ 2008,1,3,4,628,620,804,750,WN,448,N428WN,96,90,76,14,8,IND,BWI,515,3,17,0,,0,NA,NA,NA,NA,NA
+ 2008,1,3,4,926,930,1054,1100,WN,1746,N612SW,88,90,78,-6,-4,IND,BWI,515,3,7,0,,0,NA,NA,NA,NA,NA
+ 2008,1,3,4,1829,1755,1959,1925,WN,3920,N464WN,90,90,77,34,34,IND,BWI,515,3,10,0,,0,2,0,0,0,32
+ 2008,1,3,4,1940,1915,2121,2110,WN,378,N726SW,101,115,87,11,25,IND,JAX,688,4,10,0,,0,NA,NA,NA,NA,NA
+ 2008,1,3,4,1937,1830,2037,1940,WN,509,N763SW,240,250,230,57,67,IND,LAS,1591,3,7,0,,0,10,0,0,0,47
+ 2008,1,3,4,1039,1040,1132,1150,WN,535,N428WN,233,250,219,-18,-1,IND,LAS,1591,7,7,0,,0,NA,NA,NA,NA,NA
+
+ Understand:
+ 1.Inner join, we can see weather data is small enough to get into memory, so let's start with map side join.
+ a.Add weather data as cache file.Use day, month and year as the join key.
+ b.Use "Dest" column in flightdelays as filter, which will filter "Dest" column in flightdelays is equal to "SFO".
+ c.Get the "SFO" from arguments in main().
+ 2.Use Year,Month,DayofMonth as key,
+ use DepTime,ArrTime,UniqueCarrier,FlightNum,ActualElapsedTime,ArrDelay,DepDelay,Origin,Dest,PRCP,TMAX,TMIN  as value.
+ a.DepTime,ArrTime,UniqueCarrier,FlightNum,ActualElapsedTime,ArrDelay,DepDelay,Origin,Dest from flight_delays1?2?3.csv.
+ b.PRCP,TMAX,TMIN from sfo_weather.csv.
+ 3.Modify Year,Month,DayofMonth, ArrDelay as key. Custom output format.
+ 4.Output dir is "task1", output file is text file, fields separated by commas.
+ 5.Reducer task is two.
+
+ */
+
+
+public class Task1 {
+
+
+
+}
